26d7b3fae8c73e17658205a373d441af690c37bc
Junior <josias.junior@abbott.com>
2022-01-27 17:07:56 -0300

    teste de upload

---
+++ ds_prod_aula1.py
@@ -0,0 +1,9 @@
+def print_hi(name):
+    print(f'Hi, {name}, be welcome!')
+    idade = 2021 - 1983
+    print (f'{name}, your current age is {idade}')
+
+if __name__ == '__main__':
+    print_hi('VsCode with Python rocks \o/')
+
+print_hi('Josias')
